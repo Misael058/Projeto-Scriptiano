fc746e42861041eb424620beded58579cf81e5fc
Misael058 <147666566+Misael058@users.noreply.github.com>
2023-11-29 14:45:24 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,22 @@
+
+
+
+    function iniciou(){
+        const home=document.getElementById("home");
+        home.style.borderBottom="solid 3px white";
+    }
+
+    function inicio_artigo(){
+        const artigo=document.getElementById("artigo");
+        artigo.style.borderBottom="solid 3px white";
+    }
+  function biblioteca_inicio() {
+        let biblioteca =document.getElementById("biblia");
+        biblioteca.style.borderBottom="solid 3px white";
+
+  }
+  function sobre() {
+        let sobre =document.getElementById("sobreNos");
+        sobre.style.borderBottom="solid 3px white";
+
+  }
